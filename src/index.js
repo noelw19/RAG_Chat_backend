5c6ef0090ba2ec0013ac1eee351bebfa797b780f
noelw19 <noelw19@outlook.com>
2025-04-12 11:08:54 +1200

    add access control cors

--- src/index.js
+++ src/index.js
@@ -48,9 +48,18 @@ if (!fs.existsSync(vectorStoreDir)) {
 
 // Middleware
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://rag.meliorus.co.nz"],
     credentials: true
 }));
+
+app.use(function (req, res, next) {
+  var allowedDomains = ["http://localhost:5173", "https://rag.meliorus.co.nz"];
+  var origin = req.headers.origin;
+  if(allowedDomains.indexOf(origin) > -1){
+    res.setHeader('Access-Control-Allow-Origin', origin);
+  }
+  next();
+})
 app.use(helmet());
 app.use(express.json({limit: "50mb"}));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
